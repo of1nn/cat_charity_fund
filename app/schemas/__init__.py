c56139373040e3ddcb3848f63669183210cf86c3
Ofinn <dmitrijzajc175@gmail.com>
2024-07-22 20:57:59 +0300

    dfsag

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,9 +1,9 @@
-from .charity_project import (
+from .charity_project import ( # noqa
     CharityProjectCreate,  # noqa
     CharityProjectUpdate,  # noqa
     CharityProjectDB,  # noqa
 )
-from .donation import (
+from .donation import ( # noqa
     DonationCreate,  # noqa
     DonationDB,  # noqa
     DonationDBAll,  # noqa
